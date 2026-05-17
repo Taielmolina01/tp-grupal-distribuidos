62279322cff28c29d693e3536f685b701b5093bb
Nasher3296 <ignaciofernandez3296@gmail.com>
2026-05-17 12:56:41 -0300

    refactor: Generic batch builder

--- src/internal/common/messageprotocol/external/batch_builder.go
+++ src/internal/common/messageprotocol/external/batch_builder.go
@@ -7,66 +7,50 @@ import (
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/internal/common/transfer"
 )
 
-type AccountBatchBuilder struct {
-	records  []account.Account
-	byteSize int
-	maxBytes int
-	maxCount int
+type BatchBuilder[T any] struct {
+	records   []T
+	byteSize  int
+	maxBytes  int
+	maxCount  int
+	serialize func(*T) []byte
+	write     func(io.Writer, []T) error
 }
 
-func NewAccountBatchBuilder(maxCount, maxBytes int) *AccountBatchBuilder {
-	return &AccountBatchBuilder{maxBytes: maxBytes, maxCount: maxCount}
-}
-
-func (b *AccountBatchBuilder) TryAdd(acc account.Account) bool {
-	size := len(serializeAccountRecord(&acc))
+func (b *BatchBuilder[T]) TryAdd(record T) bool {
+	size := len(b.serialize(&record))
 	if !b.IsEmpty() && (len(b.records) >= b.maxCount || b.byteSize+size > b.maxBytes) {
 		return false
 	}
-	b.records = append(b.records, acc)
+	b.records = append(b.records, record)
 	b.byteSize += size
 	return true
 }
 
-func (b *AccountBatchBuilder) IsEmpty() bool {
+func (b *BatchBuilder[T]) IsEmpty() bool {
 	return len(b.records) == 0
 }
 
-func (b *AccountBatchBuilder) Flush(w io.Writer) error {
-	err := WriteAccountBatch(w, b.records)
+func (b *BatchBuilder[T]) Flush(w io.Writer) error {
+	err := b.write(w, b.records)
 	b.records = b.records[:0]
 	b.byteSize = 0
 	return err
 }
 
-type TransBatchBuilder struct {
-	records  []transfer.Transfer
-	byteSize int
-	maxBytes int
-	maxCount int
-}
-
-func NewTransBatchBuilder(maxCount, maxBytes int) *TransBatchBuilder {
-	return &TransBatchBuilder{maxBytes: maxBytes, maxCount: maxCount}
-}
-
-func (b *TransBatchBuilder) TryAdd(t transfer.Transfer) bool {
-	size := len(serializeTransRecord(&t))
-	if !b.IsEmpty() && (len(b.records) >= b.maxCount || b.byteSize+size > b.maxBytes) {
-		return false
+func NewAccountBatchBuilder(maxCount, maxBytes int) *BatchBuilder[account.Account] {
+	return &BatchBuilder[account.Account]{
+		maxBytes:  maxBytes,
+		maxCount:  maxCount,
+		serialize: serializeAccountRecord,
+		write:     WriteAccountBatch,
 	}
-	b.records = append(b.records, t)
-	b.byteSize += size
-	return true
 }
 
-func (b *TransBatchBuilder) IsEmpty() bool {
-	return len(b.records) == 0
-}
-
-func (b *TransBatchBuilder) Flush(w io.Writer) error {
-	err := WriteTransBatch(w, b.records)
-	b.records = b.records[:0]
-	b.byteSize = 0
-	return err
+func NewTransBatchBuilder(maxCount, maxBytes int) *BatchBuilder[transfer.Transfer] {
+	return &BatchBuilder[transfer.Transfer]{
+		maxBytes:  maxBytes,
+		maxCount:  maxCount,
+		serialize: serializeTransRecord,
+		write:     WriteTransBatch,
+	}
 }
