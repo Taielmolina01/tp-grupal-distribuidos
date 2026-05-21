fd371d7f820d5e6d00c4d32762a12c13fed21618
luchoserra <128339891+luchoserra@users.noreply.github.com>
2026-05-21 14:36:06 -0300

    Feat/join (#45)
    * feat: add generic join node
    
    * refactor: decouple join matching logic from exchange consumption
    
    * feat: handle EOF per client in join node
    
    * feat: add Q4 splitted-transfer join
    
    * fix: update variable names

---
+++ src/cmd/join/config.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"strconv"
+
+	"tp-grupal-distribuidos/internal/join"
+)
+
+func loadConfig() (join.JoinConfig, error) {
+	momPort, err := strconv.Atoi(os.Getenv("MOM_PORT"))
+	if err != nil {
+		return join.JoinConfig{}, errors.New("MOM_PORT environment variable is required and must be a number")
+	}
+
+	momHost := os.Getenv("MOM_HOST")
+	if momHost == "" {
+		return join.JoinConfig{}, errors.New("MOM_HOST environment variable is required")
+	}
+
+	outputExchange := os.Getenv("OUTPUT_EXCHANGE")
+	if outputExchange == "" {
+		return join.JoinConfig{}, errors.New("OUTPUT_EXCHANGE environment variable is required")
+	}
+
+	return join.JoinConfig{
+		MomHost:            momHost,
+		MomPort:            momPort,
+		InputExchange:      os.Getenv("INPUT_EXCHANGE"),
+		LeftInputExchange:  os.Getenv("LEFT_INPUT_EXCHANGE"),
+		RightInputExchange: os.Getenv("RIGHT_INPUT_EXCHANGE"),
+		OutputExchange:     outputExchange,
+	}, nil
+}
