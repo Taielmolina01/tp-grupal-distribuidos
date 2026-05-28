d5d248790220a09c258076df9e3075cf8f3d215d
tamolina01 <tamolina@fi.uba.ar>
2026-05-28 15:16:40 -0300

    fix: rollback on distinctFilter

--- src/internal/filter/constructors.go
+++ src/internal/filter/constructors.go
@@ -5,7 +5,9 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"tp-grupal-distribuidos/internal/common/account"
 	"tp-grupal-distribuidos/internal/common/fetcherresponse"
+	"tp-grupal-distribuidos/internal/common/normalizer"
 	"tp-grupal-distribuidos/internal/common/queryresult"
 	"tp-grupal-distribuidos/internal/common/transfer"
 	"tp-grupal-distribuidos/internal/common/worker"
@@ -71,6 +73,21 @@ func CreateDateRangeAndPaymentMethod(config FilterConfig) (worker.Worker, error)
 func CreateFilterAndSplitter(config filterandsplitter.FilterAndSplitterConfig) (worker.Worker, error) {
 	return filterandsplitter.NewFilterAndSplitter(
 		config,
+	)
+}
+
+func CreateBankDistinctFilter(config FilterConfig) (worker.Worker, error) {
+	return newDistinctFilter(
+		config,
+		func(ac1 account.Account, ac2 account.Account) bool {
+			return ac1.BankId == ac2.BankId
+		},
+		func(ac account.Account) string {
+			return normalizer.NormalizeBankID(ac.BankId)
+		},
+		func(t account.Account) string {
+			return normalizer.NormalizeBankID(t.BankId)
+		},
 	)
 }
 
